27809bd63282ea48033ab1d11d16cda945f9f10c
taejunji <qq00aa@naver.com>
2023-09-29 00:30:34 +0900

    add mouse event, variables

--- Drill06.py
+++ Drill06.py
@@ -9,10 +9,22 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
-    global running
+    global running, hand_coord
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                hand_coord.append(event.x)
+                hand_coord.append(TUK_HEIGHT - 1 - event.y)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
+
+running = True
+hand_coord = []
+now_boy_x, now_boy_y, past_boy_x, past_boy_y = 1280 // 2, 1024 // 2, 1280 // 2, 1024 // 2
+
+while running:
+
