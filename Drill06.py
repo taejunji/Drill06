da7e8d5cb3475d8ac9d3841ecaf13937b2ddf9b6
taejunji <qq00aa@naver.com>
2023-09-29 00:16:28 +0900

    add escape

--- Drill06.py
+++ Drill06.py
@@ -7,3 +7,12 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
